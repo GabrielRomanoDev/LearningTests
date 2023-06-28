bddfa8160ad7d6366f77111bee0a6d41e23bf997
Gabriel Romano <gromanoluz@gmail.com>
2023-06-28 11:48:11 -0300

    Criação do projeto do Firestore

---
+++ firestoreFirebase_Teste/firestoreFirebase_Teste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firestoreFirebase_Teste
+//
+//  Created by Gabriel Luz Romano on 28/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
